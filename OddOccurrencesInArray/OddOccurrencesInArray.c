f61ca6bea357f93b549377ab22576b221d06f8e2
Jyri Rummukainen <jrummuka@Jyris-MacBook-Pro.local>
2023-06-13 21:06:29 +0300

    OddOccurancesInArray, 100%, notes included

---
+++ OddOccurrencesInArray/OddOccurrencesInArray.c
@@ -0,0 +1,17 @@
+// didnt come up to this solution without bit of help, but notes for myself:
+// using the bitwise operator of XOR (^)
+// shift the bits with every number of array once
+// everytime same number occurs second time the bits are sifted back
+// so result should be only number which bits didnt get sifted back to 0.
+
+int solution(int A[], int N) {
+    // Implement your solution here
+    int ret = 0;
+    int i = 0;
+    while (i < N)
+    {
+        ret = ret ^ A[i];
+        i++;
+    }
+    return ret;
+}
