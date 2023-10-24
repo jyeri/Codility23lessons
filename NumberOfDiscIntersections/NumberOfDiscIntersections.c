9c938b5ba93fbd0d6b1bdcf3adb27163003ae9bd
Jyri Rummukainen <jrummuka@Jyris-MacBook-Pro.local>
2023-10-24 17:17:02 +0300

    intoruction of NumberOfDisc

---
+++ NumberOfDiscIntersections/NumberOfDiscIntersections.c
@@ -0,0 +1,25 @@
+// We draw N discs on a plane. The discs are numbered from 0 to N − 1. An array A of N non-negative integers, specifying the radiuses of the discs, is given. 
+// The J-th disc is drawn with its center at (J, 0) and radius A[J].
+// 
+// We say that the J-th disc and K-th disc intersect if J ≠ K and the J-th and K-th discs have at least one common point (assuming that the discs contain their borders).
+// 
+// The figure below shows discs drawn for N = 6 and A as follows:
+// 
+//   A[0] = 1
+//   A[1] = 5
+//   A[2] = 2
+//   A[3] = 1
+//   A[4] = 4
+//   A[5] = 0
+// 
+// 
+// There are eleven (unordered) pairs of discs that intersect, namely:
+// 
+// discs 1 and 4 intersect, and both intersect with all the other discs;
+// disc 2 also intersects with discs 0 and 3.
+
+
+int solution(int A[], int N)
+{
+
+}
