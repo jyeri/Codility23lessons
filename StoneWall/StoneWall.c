57aae69e20755c0d96cc453584fa3686e6d23676
jrummuka <jrummuka@student.hive.fi>
2023-10-25 20:36:58 +0300

    stonewall ruleset

---
+++ StoneWall/StoneWall.c
@@ -0,0 +1,16 @@
+// You are going to build a stone wall. The wall should be straight and N meters long, and its thickness should be constant; 
+// however, it should have different heights in different places. The height of the wall is specified by an array H of N positive integers. 
+// H[I] is the height of the wall from I to I+1 meters to the right of its left end. 
+// In particular, H[0] is the height of the wall's left end and H[N−1] is the height of the wall's right end.
+// 
+// The wall should be built of cuboid stone blocks (that is, all sides of such blocks are rectangular). 
+// Your task is to compute the minimum number of blocks needed to build the wall.
+
+// First idea is just forget everything i read, and thinking that everytime height changes, i need a new block, yes?
+// no...
+
+int solution(int H[], int N)
+{
+
+}
+
