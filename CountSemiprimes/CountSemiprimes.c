3b5206cffaccca71d5378e775f9df0e5129dc581
jrummuka <jrummuka@student.hive.fi>
2023-11-02 21:37:36 +0200

    CSP proof of concept works 66%, now the actul efficient approach

--- CountSemiprimes/CountSemiprimes.c
+++ CountSemiprimes/CountSemiprimes.c
@@ -41,7 +41,112 @@
 //      -> would cut down the cost of counting same primes again?
 //      -> in the end we gather the result arrays from this temporary array with simple bool check?
 
+// Variable notes:
+// N = Max number
+// M = How many in each array
+// P = *int where
+// Q = *int to
+
 struct Results solution(int N, int P[], int Q[], int M)
 {
 
+// -> allocate array of int from 0 to N?
+//      -> 0 = not checked, 1 = checked, not semiprime, 2 = semiprime?
+    int *semiprimes;
+    struct Results res;
+    int i = 0;
+    int j = 0;
+    int k = 0;
+
+    semiprimes = (int *)calloc(N * sizeof(int));
+    res.A = (int *)calloc(M * sizeof(int));
+    res.L = M;
+
+// iterate given arrays
+    while(k < M)
+    {
+        j = Q[k] - P[k];
+        while (j > 0)
+        {
+            if (semiprimes[P[i]] == 0)
+            {
+                if (check_semi(P[i]) == 2)
+                    semiprimes[P[i]] = 2;
+                else
+                    semiprimes[P[i]] = 1;
+            }
+            i++;
+            j--;
+        }
+        i = 0;
+        k++;
+    }
+// iterate once more to add checked into res A
+    i = 0;
+    while(i < M)
+    {
+    }
 }
+
+
+// easy solution as proof of concept-> without making the temp array and adding straight to result array
+// worked and got alrady 66% -> now we implement the checking array
+
+//helper function to check if number actually is semiprime
+//  int check_semi(int num)
+//  {
+//      int cnt = 0;
+//      int i = 2;
+//   
+//      // loop until find more than u primes or i > sqrt(Num)
+//      while (cnt < 2 && i * i <= num)
+//      {
+//          while (num % i == 0)
+//          {
+//              num /= i;
+//              ++cnt;
+//          }
+//          i++;
+//      }
+//      // If number is greater than 1, add it to
+//      // the count variable as it indicates the
+//      // number remain is prime number
+//      if (num > 1)
+//          ++cnt;
+//   
+//      // return cnt, if its 2 we all gucci
+//      return cnt;
+//  }
+//  
+//  struct Results solution(int N, int P[], int Q[], int M)
+//  {
+//  
+//      struct Results res;
+//      int i = 0;
+//      int j = 0;
+//      int k = 0;
+//  
+//      res.A = (int *)calloc(M * sizeof(int), sizeof(int));
+//      res.M = M;
+//  
+//  // iterate given arrays
+//      while(k < M)
+//      {
+//          j = Q[k] - P[k];
+//          while (j >= 0)
+//          {
+//  //            printf("%d under check. \n", P[i] + j);
+//              if (check_semi(P[i] + j) == 2)
+//              {
+//  //                printf("%d is prime. \n", P[i] + j);
+//                  res.A[k] += 1;
+//              }
+//              j--;
+//          }
+//  //        printf("\n NEXT INDEX \n");
+//          i++;
+//          k++;
+//      }
+//  
+//      return res;
+//  }
