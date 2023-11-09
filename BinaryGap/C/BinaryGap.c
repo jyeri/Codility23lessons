9cffe79a5993bae4edccf9bad216481d44043ace
jrummuka <jrummuka@student.hive.fi>
2023-11-09 06:46:15 +0200

    100% BinaryGap in C, small update, notes

--- BinaryGap/C/BinaryGap.c
+++ BinaryGap/C/BinaryGap.c
@@ -6,23 +6,24 @@
 // -> update the max value if its lower than new current
 // -> only update when there has already been start point, and its now at end point.
 
+// small update: no need to use array since we go number by number, used variable of rem instead
+
 int solution(int N) 
 {
-    int arr[32];
-    int i = 0;
     int current = 0;
     int max = 0;
     int start = 0;
+    int rem = 0;
 
     while (N > 0)
     {
-        arr[i] = N % 2;
-//        printf("%d", arr[i]);
-        if (arr[i] == 0 && start == 1)
+        rem = N % 2;
+//        printf("%d", rem);
+        if (rem == 0 && start == 1)
         {
             current++;
         }
-        else if (arr[i] == 1)
+        else if (rem == 1)
         {
             if (start == 0)
             {
