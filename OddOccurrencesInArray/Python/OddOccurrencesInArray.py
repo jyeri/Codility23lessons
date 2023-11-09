4286d31d76dc9bf46fde0ffb33378f4e29c10ac5
jrummuka <jrummuka@student.hive.fi>
2023-11-09 08:21:04 +0200

    100% OCIA in Python

---
+++ OddOccurrencesInArray/Python/OddOccurrencesInArray.py
@@ -0,0 +1,8 @@
+#straight up C solution, binary
+def solution(A):
+    ret = 0
+
+    #every number in A we bit shift, result is the one that does not get sifted back
+    for i in range(len(A)):
+        ret = ret ^ A[i]
+    return ret
